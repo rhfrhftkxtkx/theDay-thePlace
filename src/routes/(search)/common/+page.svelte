0cfa778e62ca8ae2a32f4d05fff47a3ffe7e0f2f
BlueNyang <xpsj20@gmail.com>
2025-09-15 05:19:30 +0900

    fix: 카테고리가 뜨지 않는 문제 해결
    데이터를 awit하고, drived를 이용해 데이터의 변화를 감지하도록 수정했습니다.

--- src/routes/(search)/common/+page.svelte
+++ src/routes/(search)/common/+page.svelte
@@ -17,6 +17,7 @@
 
   // 로딩 상태 저장
   let isLoading: boolean = $state(false);
+  let categoryListLoading: boolean = $state(false);
 
   // 검색 요청에 따른 json 응답 데이터를 searchedCcbaItems와 searchedMuseumItems에 저장
   async function formatSearchedResponse(data: ServerResponse): Promise<void> {
@@ -28,6 +29,22 @@
   // 클릭 시, searchKeyword와 searchFilter를 사용하여 검색 요청
   async function handleSubmitClick(): Promise<void> {
     isLoading = true;
+
+    let isCcbaSelected = undefined;
+    let isMuseumSelected = undefined;
+
+    if ($searchFilter) {
+      const ccbaFilter = $searchFilter.find((filter) => filter.code === 'ccba');
+      const museumFilter = $searchFilter.find(
+        (filter) => filter.code === 'museum'
+      );
+      if (ccbaFilter && ccbaFilter.item.length > 0) {
+        isCcbaSelected = true;
+      } else if (museumFilter && museumFilter.item.length > 0) {
+        isMuseumSelected = true;
+      }
+    }
+
     try {
       // Svelte server side에 서버로 검색 요청
       // 검색어와 필터를 JSON 형태로 서버에 POST 요청
@@ -39,6 +56,8 @@
         body: JSON.stringify({
           searchKeyword: $searchKeyword,
           searchFilter: $searchFilter,
+          searchCcba: isCcbaSelected,
+          searchMuseum: isMuseumSelected,
           pageNo: 1,
         }),
       }).then(async (response) => {
@@ -47,6 +66,7 @@
         }
         // 검색 요청이 성공하면 응답을 JSON으로 파싱
         const responseData: ServerResponse = await response.json();
+
         // 파싱된 응답 데이터를 포맷팅 및 저장
         await formatSearchedResponse(responseData);
       });
@@ -59,10 +79,23 @@
   }
 
   async function setDefaultLists(): Promise<void> {
-    ccbaList.set(await fetch('/api/khs/categories').then((res) => res.json()));
-    visitKorAreaCode2.set(
-      await fetch('/api/visitKor/areaCode').then((res) => res.json())
-    );
+    categoryListLoading = true;
+    const ccbaResponse = await fetch('/api/khs/categories');
+    if (!ccbaResponse.ok) {
+      console.error('Failed to fetch ccba categories');
+    } else {
+      const ccbaCategories = await ccbaResponse.json();
+      console.log('Fetched ccba categories:', ccbaCategories);
+      ccbaList.set(ccbaCategories);
+    }
+
+    const visitKorResponse = await fetch('/api/visitKor/areaCode');
+    if (!visitKorResponse.ok) {
+      console.error('Failed to fetch visitKor area codes');
+    } else {
+      visitKorAreaCode2.set(await visitKorResponse.json());
+    }
+    categoryListLoading = false;
   }
 
   function handleKeydown(event: KeyboardEvent): void {
@@ -104,7 +137,10 @@
   </div>
 
   <!-- Search Menu -->
-  <CategorySelector />
+  <CategorySelector
+    searchedCallback={handleSubmitClick}
+    isLoading={categoryListLoading}
+  />
   <!-- <SearchMenu isLoading={true} handleSubmitClick={async () => {}} /> -->
 
   <!-- 검색 결과 컨테이너 -->
