ff82a57012e2a3d53336143dd8047f1f8a71b69e
BlueNyang <xpsj20@gmail.com>
2025-09-18 22:53:31 +0900

    fix: env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,21 @@
 // src/hooks.server.ts
+import { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } from '$env/static/private';
 import { createServerClient } from '@supabase/ssr';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// console.log('서버가 사용 중인 Supabase URL:', env.SUPABASE_URL);
-	event.locals.supabase = createServerClient(
-		import.meta.env.SUPABASE_URL,
-		import.meta.env.SUPABASE_ANON_KEY,
-		{
-			cookies: {
-				get: (key) => event.cookies.get(key),
-				set: (key, value, options) => {
-					event.cookies.set(key, value, { ...options, path: '/' });
-				},
-				remove: (key, options) => {
-					event.cookies.delete(key, { ...options, path: '/' });
-				}
+	console.log('서버가 사용 중인 Supabase URL:', VITE_SUPABASE_URL);
+	event.locals.supabase = createServerClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY, {
+		cookies: {
+			get: (key) => event.cookies.get(key),
+			set: (key, value, options) => {
+				event.cookies.set(key, value, { ...options, path: '/' });
+			},
+			remove: (key, options) => {
+				event.cookies.delete(key, { ...options, path: '/' });
 			}
 		}
-	);
+	});
 
 	event.locals.getSession = async () => {
 		const {
